d40a81e8456afef07179e90dda99f8f5fe58edb6
momeaizi <mohamedtahameaizi@gmail.com>
2024-11-18 15:41:22 +0100

    socketio needs auth

--- mc-api/app/main/controllers/socket_event_controller.py
+++ mc-api/app/main/controllers/socket_event_controller.py
@@ -1,8 +1,8 @@
-from flask_socketio import SocketIO, join_room, leave_room
+# from flask_socketio import 
+from flask_socketio import emit
 from app.main import socketio
 from flask import request
 from loguru import logger
-# from app.main.services.socket_service import SocketService
 
 # Initialize the socket service
 # socket_service = SocketService(socketio=None)  # socketio will be passed during app initialization
@@ -11,34 +11,14 @@ from loguru import logger
 @socketio.on('connect')
 def handle_connect():
     # user_id = request.args.get('user_id')
-    logger.info(request.sid)
-    socketio.emit('hamid', to=request.sid)
+    return False
+    logger.info(request.headers)
+    emit('hamid', {"name": "taha"}, to=request.sid)
     
     # socket_service.handle_user_connect(user_id, sid)
 
 # Handling socket disconnection
 @socketio.on('disconnect')
 def handle_disconnect():
-    logger.info("request.sid")
+    logger.info(request.headers)
     sid = request.sid
-    # pass
-    # socket_service.handle_user_disconnect(sid)
-
-# Handling send notification event
-@socketio.on('send_notification')
-def handle_send_notification(data):
-    pass
-    # recipient_id = data['recipient_id']
-    # logger.info("request.sid")
-    # message = data['message']
-    # pass
-    # socket_service.send_notification(recipient_id, message)
-
-# Handling send chat message event
-@socketio.on('send_message')
-def handle_send_message(data):
-    sender_id = data['sender_id']
-    recipient_id = data['recipient_id']
-    message = data['message']
-    pass
-    # socket_service.send_message(sender_id, recipient_id, message)
