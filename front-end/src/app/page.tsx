3fbecebbde6b25514b438ded4a002525414bcf7b
Abdelmounim Skerba <mskerba@c3r3p2.1337.ma>
2024-11-22 11:06:12 +0100

    adding navbar in landing page

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -4,10 +4,27 @@ import Image from 'next/image';
 export default function LandingPage() {
   return (
     <div
-      className="h-screen bg-cover bg-center"
+      className="h-screen bg-cover bg-center grid grid-cols-1 grid-rows-[72px_1fr] gap-0"
       style={{ backgroundImage: "url('/dark_background.jpg')" }}
     >
-      fadsfasdf
+      <div className="flex flex-row justify-between	items-center	px-8">
+        <Image
+          src="/matcha_logo.png"
+          width={500}
+          height={500}
+          alt="logo"
+        />
+        <p>Log in</p>
+      </div>
+      <div className="flex flex-col justify-center	items-center">
+        <h1 className="text-5xl font-bold mb-4">
+        No boss here, change it
+        </h1>
+        <h3 className="text-xl text-gray-300">
+        He who has no skill should return to his mother's teaching
+        </h3>
+        <p>Create account</p>
+      </div>
     </div>
   );
 }
